0de9f39395b84e0063c0788e1db3de110e0d5491
sidneyarepp <sidneyarepp@outlook.com>
2021-08-01 14:19:07 -0400

    loading build folder

---
+++ build/precache-manifest.3e74f2c0496c05bdf5c4bb23a79468ea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b207b07dd605b3b7448c73d5b9031f99",
+    "url": "/PomodoroProject/index.html"
+  },
+  {
+    "revision": "73a1010f5416e016b481",
+    "url": "/PomodoroProject/static/css/main.cb9e985a.chunk.css"
+  },
+  {
+    "revision": "8f60e831c49dd590640b",
+    "url": "/PomodoroProject/static/js/2.0acf020f.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/PomodoroProject/static/js/2.0acf020f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73a1010f5416e016b481",
+    "url": "/PomodoroProject/static/js/main.ca099215.chunk.js"
+  },
+  {
+    "revision": "9af5edddcbbe3666c9cb",
+    "url": "/PomodoroProject/static/js/runtime-main.6ef5c651.js"
+  }
+]);
